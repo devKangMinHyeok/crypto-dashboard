83979fb27f42d30b8179f1d3eddd3af69ab67e4a
Kang MinHyeok <rkdalsgur032@gmail.com>
2022-02-07 16:36:09 +0900

    state 예제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-function Hello({ color }) {
-  return <div style={(color = { color })}>Hello</div>;
+import { useState } from "react";
+
+function Hello() {
+  const [number, setNumber] = useState(0);
+  const handleChange = (evt) => {
+    setNumber(evt.target.value);
+  };
+  return (
+    <>
+      <div>{number}</div>
+      <input type="number" value={number} onChange={handleChange} />
+    </>
+  );
 }
 
 function App() {
   return (
     <>
-      <Hello color="red" />
-      <Hello color="blue" />
+      <Hello />
     </>
   );
 }
