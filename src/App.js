e525046350c93bac23c40810ebf5c10634a8ed7d
Kang MinHyeok <rkdalsgur032@gmail.com>
2022-02-22 16:26:07 +0900

    데이터를 받아와 화면에 보여주기 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 function BtcInfo() {
   const [infoLoading, setInfoLoading] = useState(true);
   const [info, setInfo] = useState();
+  const [refresh, setRefresh] = useState(false);
   useEffect(async () => {
     const response = await fetch(
       "https://api.coinpaprika.com/v1/tickers/btc-bitcoin"
@@ -10,10 +11,27 @@ function BtcInfo() {
     const data = await response.json();
     setInfo(data);
     setInfoLoading(false);
-  }, []);
+  }, [refresh]);
+  if (info) console.log(info);
+  const handleClick = () => {
+    setInfoLoading(true);
+    setRefresh((prev) => !prev);
+  };
   return (
     <>
-      <div>{infoLoading ? "Loading" : info.quotes.USD.ath_price}</div>
+      {infoLoading ? (
+        <div>Loading</div>
+      ) : (
+        <div>
+          <div>역대최고가</div>
+          <div>{info.quotes.USD.ath_price}</div>
+          <div>시가총액</div>
+          <div>{info.quotes.USD.market_cap}</div>
+          <div>시가총액 변화율</div>
+          <div>{info.quotes.USD.market_cap_change_24h}</div>
+        </div>
+      )}
+      <button onClick={handleClick}>Refresh</button>
     </>
   );
 }
